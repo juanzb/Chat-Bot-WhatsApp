f8e11e60ff4b6cb986ece66aea8e391b8ac16c9e
juanzb <juanzarache2000@gmail.com>
2025-08-29 01:52:12 -0500

    fix code chat Bot WhatsApp-web-js-linux, test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+    executablePath: "/snap/bin/chromium",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
 });
